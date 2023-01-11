3c093955f179f8d9810acf8f9a9f764c4b9ad569
094-gengar <koyamagen.genkoyama@gmail.com>
2023-01-11 21:28:41 +0900

    [add] PrimeFactorPollard, RollingHash

---
+++ cpp/math/PrimeFactorPollard.hpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+namespace m9 {
+using ll = long long;
+
+template<class T> T PM(T X, T N, T M)
+{
+	T ret{1};
+	while(N > 0)
+	{
+		if(N & 1) { (ret *= X) %= M; }
+		(X *= X) %= M;
+		N >>= 1;
+	}
+	return ret;
+}
+
+bool IP(ll N)
+{
+	if(N <= 1) { return false; }
+	if(N == 2 || N == 3) { return true; }
+	if(N % 2 == 0) { return false; }
+	auto A = std::vector<ll>{2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+	ll s{}, d{N - 1};
+	while(d % 2 == 0) { s++; d >>= 1; }
+	for(const auto& a : A)
+	{
+		if(a % N == 0) { return true; }
+		ll t, x = PM<__int128_t>(a, d, N);
+		if(x != 1)
+		{
+			for(t = 0; t < s; t++)
+			{
+				if(x == N - 1) { break; }
+				x = (__int128_t)(x) * x % N;
+			}
+			if(t == s) { return false; }
+		}
+	}
+	return true;
+}
+
+ll pollard(ll N)
+{
+	if(N % 2 == 0)return 2;
+	if(IP(N)) { return N; }
+	auto f = [&](ll x) -> ll {
+		return ((__int128_t(x) * x + 1) % N);
+	};
+	ll step{};
+	while(true)
+	{
+		step++;
+		ll x = step;
+		ll y = f(x);
+		while(true)
+		{
+			ll p = std::gcd(y - x + N, N);
+			if(p == 0 || p == N) { break; }
+			if(p != 1) { return p; }
+			x = f(x);
+			y = f(f(y));
+		}
+	}
+}
+
+std::vector<ll> primeFactPollard(ll N)
+{
+	if(N == 1) { return std::vector<ll>{}; }
+	ll p = pollard(N);
+	if(p == N) { return std::vector<ll>{p}; }
+	std::vector<ll> L = primeFactPollard(p), R = primeFactPollard(N / p);
+	L.insert(std::end(L), std::begin(R), std::end(R));
+	std::sort(std::begin(L), std::end(L));
+	return L;
+}
+}
---
+++ cpp/structure/RollingHash.hpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <chrono>
+#include <cassert>
+#include <cmath>
+
+namespace m9 {
+
+using ll = long long;
+
+template<class T> struct RollingHash {
+private:
+	int N;
+	const ll MOD = (1ll << 61) - 1;
+	ll base;
+	std::vector<ll> Hs{}, Pw{};
+	ll mul(ll x, ll y) const { return (__int128_t)(x) * y % MOD; }
+public:
+	RollingHash() = default;
+	RollingHash(const T& s, ll base = -1) : N(s.size()) {
+		if(base == -1){ base = std::chrono::duration_cast<std::chrono::microseconds> \
+			(std::chrono::system_clock::now().time_since_epoch()).count(); }
+		Hs.assign(N + 1, 0); Pw.assign(N + 1, 1);
+		for(int i{}; i < N; i++)
+		{
+			Hs[i + 1] = (mul(Hs[i], base) + s[i]) % MOD;
+			Pw[i + 1] = (mul(Pw[i], base)) % MOD;
+		}
+	}
+
+	ll get(const int l, const int r) const
+	{
+		assert(l <= r and 0 <= l and r <= N);
+		return (Hs[r] - mul(Hs[l], Pw[r - l]) % MOD + MOD) % MOD;
+	}
+
+	ll getAll() const { return get(0, N);}
+
+	int getLCP(const int a, const int b) const
+	{
+		assert(a >= 0 and b >= 0 and a < N and b < N);
+		int ok{0}, ng{std::min(N - a, N - b)};
+		while(abs(ok - ng) > 1)
+		{
+			int mid = (ok + ng) / 2;
+			(get(a, a + mid) != get(b, b + mid) ? ng : ok) = mid;
+		}
+		return ok;
+	}
+};
+}
