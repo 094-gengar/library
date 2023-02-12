1ed5a0b78f9aaacf676a6e2eb07d69459827fce4
094-gengar <koyamagen.genkoyama@gmail.com>
2023-02-12 11:45:10 +0900

    [add] gridtoInt.hpp

---
+++ cpp/other/gridtoInt.hpp
@@ -0,0 +1,17 @@
+#include <utility>
+
+namespace m9 {
+
+template<class T> auto GridtoT(const T width, const T x, const T y) -> T {
+	return x * width + y;
+}
+
+template<class T> auto GridtoT(const T width, const std::pair<T, T> p) -> T {
+	return p.first * width + p.second;
+}
+
+template<class T> auto TtoGrid(const T width, const T N) -> std::pair<T, T> {
+	return std::make_pair(N / width, N % width);
+}
+
+} // namespace m9
